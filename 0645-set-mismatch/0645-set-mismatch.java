759f3044ae09e4787f5013704871c60bd24beb9d
Tanisha Chakraborty <tanishachakraborty309@gmail.com>
2026-08-18 22:40:13 +0530

    Time: 15 ms (24.52%), Space: 48.7 MB (6.67%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+        int duplicate = -1;
+        int missing = -1;
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                duplicate = nums[i];
+            }
+            if(nums[i+1] - nums[i] > 1){
+                missing = nums[i] + 1;
+            }
+        }
+        if(nums[0]!=1){
+            missing = 1;
+        }
+        if(nums[nums.length-1] != nums.length){
+            missing = nums.length;
+        }
+        return new int[]{duplicate , missing};
+    }
+}
