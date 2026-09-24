f1e25c2855574d6ddf3985538fafcdd4530e4214
Tanisha Chakraborty <tanishachakraborty309@gmail.com>
2026-09-25 00:30:54 +0530

    Time: 4 ms (65.81%), Space: 268.6 MB (38.92%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -11,7 +11,7 @@ class Solution {
         
     }
     public void reverse(int start, int end, int nums[]){
-        while(start<=end){
+        while(start<end){
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
